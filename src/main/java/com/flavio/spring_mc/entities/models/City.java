449f5c669e6a1656ed9cdf5eae65174ff5c21576
Tana-McFarlane <elisetana91@gmail.com>
2025-05-29 14:04:03 -0700

    Update models

---
+++ src/main/java/com/flavio/spring_mc/entities/models/City.java
@@ -0,0 +1,84 @@
+package com.flavio.spring_mc.entities.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_city")
+public class City implements Serializable{
+    @Serial
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    public City() {
+    }
+
+    public City(Integer id, String name, State state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City city)) return false;
+
+        if (getId() != null ? !getId().equals(city.getId()) : city.getId() != null) return false;
+        if (getName() != null ? !getName().equals(city.getName()) : city.getName() != null) return false;
+        return getState() != null ? getState().equals(city.getState()) : city.getState() == null;
+    }
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getState() != null ? getState().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", estado=" + state +
+                '}';
+    }
+
+}
