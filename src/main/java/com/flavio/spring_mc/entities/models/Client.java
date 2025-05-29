b41a3957d2753f8c8e759bfee1c8dcc3160ee02e
Tana-McFarlane <elisetana91@gmail.com>
2025-05-29 14:05:19 -0700

    Update Models

---
+++ src/main/java/com/flavio/spring_mc/entities/models/Client.java
@@ -0,0 +1,141 @@
+package com.flavio.spring_mc.entities.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flavio.spring_mc.entities.enums.TypeClient;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String mail;
+    private String cpfOrCnpj;
+    private Integer typeClient;
+
+    @ElementCollection
+    @CollectionTable(name = "tb_phone")
+    private Set<String> phones = new HashSet<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Address> addresses = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public Client() {
+    }
+
+    public Client(Integer id, String name, String mail, String cpfOrCnpj, TypeClient typeClient) {
+        this.id = id;
+        this.name = name;
+        this.mail = mail;
+        this.cpfOrCnpj = cpfOrCnpj;
+        this.typeClient = typeClient.getCod();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCpfOrCnpj() {
+        return cpfOrCnpj;
+    }
+
+    public void setCpfOrCnpj(String cpfOrCnpj) {
+        this.cpfOrCnpj = cpfOrCnpj;
+    }
+
+    public TypeClient getTypeClient() {
+        return TypeClient.toEnum(typeClient);
+    }
+
+    public void setTypeClient(TypeClient typeClient) {
+        this.typeClient = typeClient.getCod();
+    }
+
+    public Set<String> getPhones() {
+        return phones;
+    }
+
+    public void addPhones(String phone) {
+        getPhones().add(phone);
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void addAddress(Address address) {
+        getAddresses().add(address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category client)) return false;
+
+        if (getId() != null ? !getId().equals(client.getId()) : client.getId() != null) return false;
+        return getName() != null ? getName().equals(client.getName()) : client.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mail='" + mail + '\'' +
+                ", cpfOrCnpj='" + cpfOrCnpj + '\'' +
+                ", typeClient=" + typeClient +
+                ", phones=" + phones +
+                ", addresses=" + addresses +
+                '}';
+    }
+}
