799bbaad0a0858125b6bf166a5cd0d37ed5b7562
Tana-McFarlane <elisetana91@gmail.com>
2025-06-05 07:38:41 -0700

    Update Models

---
+++ src/main/java/com/flavio/spring_mc/entities/models/State.java
@@ -0,0 +1,78 @@
+package com.flavio.spring_mc.entities.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_state")
+public class State implements Serializable {
+    @Serial
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @OneToMany(mappedBy = "state")
+    private List<City> cities = new ArrayList<>();
+
+    public State() {
+    }
+
+    public State(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<City> getCity() {
+        return cities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof State state)) return false;
+
+        if (getId() != null ? !getId().equals(state.getId()) : state.getId() != null) return false;
+        if (getName() != null ? !getName().equals(state.getName()) : state.getName() != null) return false;
+        return getCity() != null ? getCity().equals(state.getCity()) : state.getCity() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getCity() != null ? getCity().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cidades=" + cities +
+                '}';
+    }
+}
