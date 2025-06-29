3c765207c70085f9028544c93402a861a3335dd1
Tana-McFarlane <elisetana91@gmail.com>
2025-06-29 09:12:48 -0700

    Update services

---
+++ src/main/java/com/flavio/spring_mc/services/CategoryService.java
@@ -0,0 +1,35 @@
+package com.flavio.spring_mc.services;
+
+import com.flavio.spring_mc.entities.models.Category;
+import com.flavio.spring_mc.repository.CategoryRepository;
+import com.flavio.spring_mc.services.exceptions.ObjectNotFoundExcepetion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public Category insertCategory(Category cat){
+        return categoryRepository.save(cat);
+    }
+
+    public Category findById(Integer id){
+        Optional<Category> catOpt = categoryRepository.findById(id);
+        return catOpt.orElseThrow(() -> new ObjectNotFoundExcepetion("Object not found"));
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category updateCategory(Category category) {
+      findById(category.getId());
+      return categoryRepository.save(category);
+    }
+}
